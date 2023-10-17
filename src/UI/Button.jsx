356d589c5dab6eab222d5729ece353b2873cfa76
Sugan T <t.suganeeshwaran@gmail.com>
2023-10-17 23:48:26 +0530

    Building the Cart page and Overview with Redux

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "group relative inline-block items-center justify-start overflow-hidden rounded-full bg-yellow-500 font-medium transition-all hover:bg-white";
   const styles = {
@@ -13,11 +14,21 @@ function Button({ children, disabled, to, type }) {
   if (to)
     return (
       <Link to={to} className={styles[type]}>
-        <span className="absolute inset-0 rounded-full border-0 border-white transition-all duration-100 ease-linear group-hover:border-[25px]"></span>
+        <span className="absolute inset-0  rounded-full border-0 border-white transition-all duration-100 ease-linear group-hover:border-[25px]"></span>
         <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-stone-800">
           {children}
         </span>
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        <span className="absolute inset-0 rounded-full border-0 border-white transition-all duration-100 ease-linear group-hover:border-[25px]"></span>
+        <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-stone-800">
+          {children}
+        </span>
+      </button>
     );
 
   return (
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: "Mediterranean",
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    incItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  incItemQuantity,
+  decItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getUsername = (state) => state.user.username;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
