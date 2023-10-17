d2be08cba2d9d56dbb35dc36b836144fa2553dbe
Sugan T <t.suganeeshwaran@gmail.com>
2023-10-17 13:31:02 +0530

    Modeling The " state with React Toolkit

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { createOrder } from "../../services/apiRestaurant";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,7 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
+  const username = useSelector((state) => state.user.username);
   const cart = fakeCart;
 
   return (
@@ -54,6 +56,7 @@ function CreateOrder() {
               className="input w-full"
               type="text"
               name="customer"
+              defaultValue={username}
               required
             />
           </div>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function username() {
-  return <div className="hidden text-sm font-semibold md:block">sugan</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default username;
