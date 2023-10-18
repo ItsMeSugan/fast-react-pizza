83c0e8e838d3a193a1a95fb8231bf38fb60faa40
Sugan T <t.suganeeshwaran@gmail.com>
2023-10-18 13:11:42 +0530

    Deleting and Updating the Cart and the Menu items

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -7,6 +7,7 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "group relative inline-block items-center justify-start overflow-hidden rounded-full bg-gray-600 font-medium transition-all hover:bg-white px-4 py-3 md:px-6 md:py-4",
   };
